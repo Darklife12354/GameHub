4b9d9168917189c49f11fc8d9f9e942907b4fb5c
Darklife12354 <119098295+Darklife12354@users.noreply.github.com>
2023-05-05 13:33:58 +0000

    test

--- app/sw.js
+++ app/sw.js
@@ -1,6 +1,6 @@
 const CACHE = "pwabuilder-offline-page";
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-const offlineFallbackPage = "app/offline.html";
+const offlineFallbackPage = "offline.html";
 self.addEventListener("message", (event) => {
     if (event.data && event.data.type === "SKIP_WAITING") {
         self.skipWaiting();
